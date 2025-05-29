e63a1db81be4f7d2d49600863c2fd814ffd0dd65
Pavel Solovev <daron439@gmail.com>
2025-05-29 22:22:47 +0200

    Add workflow to prune old packages

---
+++ prune.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/bash
+while getopts 'd' opt; do
+    case "$opt" in
+        d) DRY_RUN=true ;;
+        *) echo "Usage $0 [-d]" >&2; exit 1
+    esac
+done
+shift "$((OPTIND-1))"
+
+export GH_PAGER=
+DAYS_OLD=30
+
+mapfile -t packages < <(cat <(yq -r '.jobs | keys[]' < .github/workflows/packages.yml) \
+                            <(yq -r '.jobs | keys[]' < .github/workflows/packages-gear.yml) \
+                            | sort -u | grep -v 'changed-files')
+
+packages+=(buildroot kde-unstable)
+
+now=$(date +%s)
+
+for pkg in "${packages[@]}"; do
+    gh api --paginate -H "Accept: application/vnd.github+json" \
+        "/users/solopasha/packages/container/kde6-copr%2F$pkg/versions" | \
+    jq -c '.[]' | while read -r version; do
+
+        id=$(echo "$version" | jq -r '.id')
+        tags=$(echo "$version" | jq -r '.metadata.container.tags | join(",")')
+        updated_at=$(echo "$version" | jq -r '.updated_at')
+        updated_epoch=$(date -d "$updated_at" +%s)
+        age_days=$(( (now - updated_epoch) / 86400 ))
+        pkgname="${pkg//%2F//}"
+
+        should_delete=false
+
+        if [[ -z "$tags" ]]; then
+          echo "🗑 Deleting untagged $pkgname $id"
+          should_delete=true
+        fi
+
+        if echo "$tags" | grep -Eq '\bpr-[0-9]+\b'; then
+          pr_num=$(echo "$tags" | grep -o '[0-9]\+')
+          state=$(gh api "/repos/solopasha/kde6-copr/pulls/$pr_num" -q '.state')
+          if [[ "$state" == "closed" ]]; then
+            echo "🗑 Deleting PRs artifacts: $pkgname $tags (ID: $id)"
+            should_delete=true
+          else
+            echo "⏩ Skipping PR-$pr_num (still open) in $pkgname: $tags"
+            continue
+          fi
+        fi
+
+        if [[ "$tags" =~ latest ]]; then
+          # echo "⏩ Skipping: $pkgname $tags (contains 'latest')"
+          continue
+        fi
+
+        if [[ "$age_days" -ge "$DAYS_OLD" ]]; then
+          echo "🗑 Deleting: $pkgname $tags (ID: $id, $age_days days old)"
+          should_delete=true
+        fi
+
+        if [[ "$should_delete" == true ]]; then
+            if [[ "$DRY_RUN" == "true" ]]; then
+                echo "💤 Dry-run: would delete /users/solopasha/packages/container/kde6-copr%2F$pkg/versions/$id"
+            else
+                gh api -X DELETE "/users/solopasha/packages/container/kde6-copr%2F$pkg/versions/$id"
+            fi
+        fi
+    done
+done
